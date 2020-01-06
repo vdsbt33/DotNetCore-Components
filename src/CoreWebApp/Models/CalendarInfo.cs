21e10368e77fc61fe5a8a1773db667bc6fe304e3
Vinícius de Souza <vdsbt33@gmail.com>
2020-01-05 21:51:51 -0300

    Improved calendar.

---
+++ src/CoreWebApp/Models/CalendarInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApp.Models
+{
+    public class CalendarInfo
+    {
+        /// <summary>
+        /// Currently selected date
+        /// </summary>
+        public DateTime SelectedDate { get; set; }
+
+        /// <summary>
+        /// Highlights the day of the SelectedDate variable
+        /// </summary>
+        public bool HighlightDay { get; set; }
+
+        public CalendarInfo()
+        {
+
+        }
+
+        public CalendarInfo(DateTime SelectedDate, bool HighlightDay )
+        {
+            this.SelectedDate = SelectedDate;
+            this.HighlightDay = HighlightDay;
+        }
+    }
+}
--- src/CoreWebApp/wwwroot/js/site.js
+++ src/CoreWebApp/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    $('.calendar').each(function (calendar) {
+        alert('The calendar ' + $(calendar).attr('id') + ' has ' + $(calendar).find('.content').find('.item').length + ' items.');
+        //$(calendar).find('.content .item').click(function (day) {
+        //    alert('Clicked calendar day');
+        //    $(calendar).find('.calendarAction').prop('display', 'block');
+        //});
+    });
+});
