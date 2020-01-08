d86e9446bb75d11f6693b4b48a8fa2deebc13630
Vinícius de Souza <vdsbt33@gmail.com>
2020-01-08 14:01:28 -0300

    Added calendar action.

--- src/CoreWebApp/wwwroot/js/site.js
+++ src/CoreWebApp/wwwroot/js/site.js
@@ -1,9 +1,24 @@
 ﻿$(document).ready(function () {
-    $('.calendar').each(function (calendar) {
-        alert('The calendar ' + $(calendar).attr('id') + ' has ' + $(calendar).find('.content').find('.item').length + ' items.');
-        //$(calendar).find('.content .item').click(function (day) {
-        //    alert('Clicked calendar day');
-        //    $(calendar).find('.calendarAction').prop('display', 'block');
-        //});
+    $(window).click(function (elem) {
+        if (!$(elem.target).hasClass('calendarAction'))
+            closeCalendarDetails();
+    });
+
+    $('.calendar').each(function (id, calendarElem) {
+        //alert('The calendar ' + $(this).attr('id') + ' has ' + $(this).find('.content').find('.item').length + ' items.');
+        $(this).find('.content .item').click(function () {
+            //alert('Clicked day ' + $(this).text().trim(' '));
+            var action = $(calendarElem).find('.calendarAction');
+            $(action).css('top', $(this).prop('offsetTop') - 48);
+            $(action).css('left', $(this).prop('offsetLeft') - 40);
+            action.fadeIn(100);
+        });
     });
 });
+
+
+function closeCalendarDetails() {
+    $('.calendarAction').each(function () {
+        $(this).fadeOut(200);
+    });
+}
